f192617ad45d5426f5f12a326c65502781166c4d
Smolsky-Denis <smolskyden@gmail.com>
2026-05-10 16:13:46 +0200

    updated

--- src/core/middlewares/validation/id.validation.ts
+++ src/core/middlewares/validation/id.validation.ts
@@ -4,9 +4,7 @@ import {ObjectId} from "mongodb";
 export const idValidation = [
     param("id")
     .exists().withMessage("id is required")
-    .bail()
     .notEmpty().withMessage("id cannot be empty")
-    .bail()
     .custom((id) => ObjectId.isValid(id))
     .withMessage("Invalid id format")
 ]
