6fb108bf4d147175d4ed790f7ace9dc95a8d3fde
Smolsky-Denis <smolskyden@gmail.com>
2026-05-10 11:40:33 +0200

    updated

--- src/blogs/routers/mapers/map-to-blog-view-mode.util.ts
+++ src/blogs/routers/mapers/map-to-blog-view-mode.util.ts
@@ -9,6 +9,6 @@ export const mapToBlogViewModel = (blog: WithId<BlogDBModel>) : BlogViewModel =>
         description: blog.description,
         websiteUrl: blog.websiteUrl,
         createdAt: blog.createdAt,
-        isMembership: true,
+        isMembership: false,
     }
 }
