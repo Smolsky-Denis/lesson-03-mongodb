f211319b4255d2ef9fa8ce7f2f3efefdc35b62b7
Smolsky-Denis <smolskyden@gmail.com>
2026-05-10 16:06:33 +0200

    updated

--- src/core/middlewares/validation/id.validation.ts
+++ src/core/middlewares/validation/id.validation.ts
@@ -1,10 +1,12 @@
 import { param } from "express-validator"
 import {ObjectId} from "mongodb";
 
-export const idValidation = param("id")
+export const idValidation = [
+    param("id")
     .exists().withMessage("id is required")
     .bail()
     .notEmpty().withMessage("id cannot be empty")
     .bail()
     .custom((id) => ObjectId.isValid(id))
     .withMessage("Invalid id format")
+]
